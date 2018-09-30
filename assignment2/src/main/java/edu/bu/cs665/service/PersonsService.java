5cfc13eda1c54e6a62d2a6dbca0e19de8b0a6625
Jimmy Goddard <jimmy.goddard@gmail.com>
2018-09-29 22:11:03 -0400

    Added some methods to the PersonsService

--- assignment2/src/main/java/edu/bu/cs665/service/PersonsService.java
+++ assignment2/src/main/java/edu/bu/cs665/service/PersonsService.java
@@ -1,5 +1,12 @@
 package edu.bu.cs665.service;
 
+import edu.bu.cs665.dto.persons.Employee;
+
 public interface PersonsService {
 
+  void addEmployee(Employee employee);
+
+  void updateEmployee(int id, Employee employee);
+
+  void deleteEmployee(int id);
 }
--- assignment2/src/main/java/edu/bu/cs665/service/PersonsServiceImpl.java
+++ assignment2/src/main/java/edu/bu/cs665/service/PersonsServiceImpl.java
@@ -2,6 +2,7 @@ package edu.bu.cs665.service;
 
 import edu.bu.cs665.dao.PersonsDao;
 import edu.bu.cs665.dao.PersonsDaoImpl;
+import edu.bu.cs665.dto.persons.Employee;
 
 public class PersonsServiceImpl implements PersonsService {
   private final PersonsDao personsDao = PersonsDaoImpl.getPersonsDao();
@@ -14,5 +15,20 @@ public class PersonsServiceImpl implements PersonsService {
 
   public static PersonsService getPersonsService() {
     return PersonsServiceImplHolder.instance;
+  }
+
+  @Override
+  public void addEmployee(final Employee employee) {
+    personsDao.addEmployee(employee);
+  }
+
+  @Override
+  public void updateEmployee(final int id, final Employee employee) {
+    personsDao.updateEmployee(id, employee);
+  }
+
+  @Override
+  public void deleteEmployee(final int id) {
+    personsDao.deleteEmployee(id);
   }
 }

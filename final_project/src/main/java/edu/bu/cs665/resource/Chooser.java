727b19dd0c8369d1a0b6269e3d729d654f4de7d4
Jimmy Goddard <jimmy.goddard@gmail.com>
2018-11-24 18:05:53 -0500

    - Added choosers

---
+++ final_project/src/main/java/edu/bu/cs665/resource/Chooser.java
@@ -0,0 +1,40 @@
+package edu.bu.cs665.resource;
+
+import edu.bu.cs665.dto.car.Car;
+import java.util.List;
+
+public interface Chooser {
+
+  /**
+   * Present a menu of cars to choose from
+   *
+   * @param cars the list of cars from which to choose
+   * @return the id of the chosen employee
+   */
+  String getCarChoice(List<Car> cars);
+
+  /**
+   * Present a menu of items to choose from
+   *
+   * @param choices the list of choices from which to choose
+   * @param showPrompt whether or not to show the default prompt
+   * @return the item from the list of choices which was selected
+   */
+  String getSingleChoice(List<String> choices, boolean showPrompt);
+
+  /**
+   * Preent a menu of choices to choose from
+   *
+   * @param choices the list of choices from which to choose
+   * @return the item from the list of choices which was selected
+   */
+  String getSingleChoice(List<String> choices);
+
+  /**
+   * Present a menu of choices to choose from allowing multiple selections
+   *
+   * @param choices the list of choices from which to choose
+   * @return the list selections from the list of choices
+   */
+  List<String> getMultipleChoices(List<String> choices);
+}
---
+++ final_project/src/main/java/edu/bu/cs665/resource/ChooserImpl.java
@@ -0,0 +1,67 @@
+package edu.bu.cs665.resource;
+
+import edu.bu.cs665.dto.car.Car;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ChooserImpl implements Chooser {
+
+  @Override
+  public String getCarChoice(final List<Car> cars) {
+    final Chooser chooser = new ChooserImpl();
+    return chooser.getSingleChoice(
+        cars.stream().map(Car::getSerialNumber).map(String::valueOf).collect(Collectors.toList()),
+        false);
+  }
+
+  @Override
+  public String getSingleChoice(final List<String> choices, final boolean showPrompt) {
+    final Scanner keyboard = new Scanner(System.in);
+    String selection = null;
+    while (selection == null) {
+      if (showPrompt) {
+        System.out.println("Please choose one of the following:");
+        System.out.println();
+      }
+      for (int i = 0; i < choices.size(); ++i) {
+        System.out.println(String.format("\t%d. %s", i + 1, choices.get(i)));
+      }
+      System.out.println();
+      final String prompt = String.format("Please select between 1 and %d: ", choices.size());
+      System.out.print(prompt);
+      int choice = -1;
+      try {
+        choice = keyboard.nextInt();
+      } catch (final InputMismatchException e) {
+        keyboard.reset();
+        keyboard.next();
+      }
+      if (choice < 1 || choice > choices.size()) {
+        System.out.println("I'm sorry, I did not understand your choice.");
+      } else {
+        selection = choices.get(choice - 1);
+      }
+    }
+    return selection;
+  }
+
+  @Override
+  public String getSingleChoice(final List<String> choices) {
+    return getSingleChoice(choices, true);
+  }
+
+  @Override
+  public List<String> getMultipleChoices(final List<String> choices) {
+    final List<String> selections = new ArrayList<>();
+    choices.add("done");
+    final String selection = getSingleChoice(choices);
+    if (selection.equals("done")) {
+      return selections;
+    }
+    selections.add(selection);
+    return selections;
+  }
+}
